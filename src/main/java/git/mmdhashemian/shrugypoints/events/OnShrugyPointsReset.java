d92dfe699e1256738953b8a92a1a89e1d8d3661e
Unknown <hashemian.isf@gmail.com>
2017-03-05 16:42:00 +0330

    Events classes description added.

--- src/main/java/git/mmdhashemian/shrugypoints/events/OnShrugyPointsReset.java
+++ src/main/java/git/mmdhashemian/shrugypoints/events/OnShrugyPointsReset.java
@@ -5,40 +5,46 @@ import java.util.UUID;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.HandlerList;
 
+/**
+ * Class will call when player points set to 0
+ * 
+ * @author Mohammad_Hashemian
+ *
+ */
 public class OnShrugyPointsReset extends ShrugyPointsChangeEvent implements Cancellable {
 
-	/**
-	 * Bukkit handler list.
-	 */
-	private static final HandlerList handlers = new HandlerList();
+    /**
+     * Bukkit handler list.
+     */
+    private static final HandlerList handlers = new HandlerList();
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param source
-	 *            - UUID of source player.
-	 * @param target
-	 *            - UUID of target player.
-	 * 
-	 * @param change
-	 *            - Amount of points to be changed.
-	 */
-	public OnShrugyPointsReset(UUID source, UUID target, int change) {
-		super(source, target, 0);
-	}
+    /**
+     * Constructor.
+     * 
+     * @param source
+     *            - UUID of source player.
+     * @param target
+     *            - UUID of target player.
+     * 
+     * @param change
+     *            - Amount of points to be changed.
+     */
+    public OnShrugyPointsReset(UUID source, UUID target, int change) {
+        super(source, target, 0);
+    }
 
-	/**
-	 * Static method to get HandlerList.
-	 * 
-	 * @return HandlerList.
-	 */
-	public static HandlerList getHandlerList() {
-		return handlers;
-	}
+    /**
+     * Static method to get HandlerList.
+     * 
+     * @return HandlerList.
+     */
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 
-	@Override
-	public HandlerList getHandlers() {
-		return handlers;
-	}
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 
 }
--- src/main/java/git/mmdhashemian/shrugypoints/events/ShrugyPointsChangeEvent.java
+++ src/main/java/git/mmdhashemian/shrugypoints/events/ShrugyPointsChangeEvent.java
@@ -7,101 +7,107 @@ import org.bukkit.event.HandlerList;
 
 import org.bukkit.event.Cancellable;
 
+/**
+ * Shrugy change event handler
+ * 
+ * @author Mohammad_Hashemian
+ *
+ */
 public class ShrugyPointsChangeEvent extends Event implements Cancellable {
 
-	/**
-	 * Bukkit Handler list
-	 */
-	private static final HandlerList handlers = new HandlerList();
-	/**
-	 * sourceID = who sent the points. targetID = who received the points.
-	 */
-	private final UUID sourceID, targetID;
-	/**
-	 * Amount of points being changed by sourceID
-	 */
-	private int change;
-	/**
-	 * Cancelled flag.
-	 */
-	private boolean cancelled;
+    /**
+     * Bukkit Handler list
+     */
+    private static final HandlerList handlers = new HandlerList();
+    /**
+     * sourceID = who sent the points. targetID = who received the points.
+     */
+    private final UUID sourceID, targetID;
+    /**
+     * Amount of points being changed by sourceID
+     */
+    private int change;
+    /**
+     * Cancelled flag.
+     */
+    private boolean cancelled;
 
-	/**
-	 * Public constructor.
-	 * 
-	 * @param source
-	 *            - Point sender UUID
-	 * @param target
-	 *            - Point receiver UUID
-	 * @param change
-	 *            - Amount of change applied to current points.
-	 */
-	public ShrugyPointsChangeEvent(UUID source, UUID target, int change) {
-		this.sourceID = source;
-		this.targetID = target;
-		this.change = change;
-	}
+    /**
+     * Public constructor.
+     * 
+     * @param source
+     *            - Point sender UUID
+     * @param target
+     *            - Point receiver UUID
+     * @param change
+     *            - Amount of change applied to current points.
+     */
+    public ShrugyPointsChangeEvent(UUID source, UUID target, int change) {
+        this.sourceID = source;
+        this.targetID = target;
+        this.change = change;
+    }
 
-	/**
-	 * Get amount of points applied to previous points.
-	 * 
-	 * @return - Amount of change.
-	 */
-	public int getChange() {
-		return change;
-	}
+    /**
+     * Get amount of points applied to previous points.
+     * 
+     * @return - Amount of change.
+     */
+    public int getChange() {
+        return change;
+    }
 
-	/**
-	 * Set the amount of change that will be used to adjust the player's
-	 * balance.
-	 * 
-	 * @param change
-	 *            - Amount of change.
-	 */
-	public void setChange(int change) {
-		this.change = change;
-	}
+    /**
+     * Set the amount of change that will be used to adjust the player's
+     * balance.
+     * 
+     * @param change
+     *            - Amount of change.
+     */
+    public void setChange(int change) {
+        this.change = change;
+    }
 
-	/**
-	 * Get Source UUID if not null ...!
-	 * 
-	 * @return Source player UUID if not null (Console)
-	 */
-	public UUID getSourceID() {
-		return sourceID;
-	}
+    /**
+     * Get Source UUID if not null ...!
+     * 
+     * @return Source player UUID if not null (Console)
+     */
+    public UUID getSourceID() {
+        return sourceID;
+    }
 
-	/**
-	 * Get Target UUID.
-	 * 
-	 * @return Target player UUID
-	 */
-	public UUID getTargetID() {
-		return targetID;
-	}
+    /**
+     * Get Target UUID.
+     * 
+     * @return Target player UUID
+     */
+    public UUID getTargetID() {
+        return targetID;
+    }
 
-	@Override
-	public boolean isCancelled() {
-		return cancelled;
-	}
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
 
-	@Override
-	public void setCancelled(boolean cancelled) {
-		this.cancelled = cancelled;
-	}
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
 
-	/**
-	 * Static method to get HandlerList.
-	 * 
-	 * @return HandlerList.
-	 */
-	public static HandlerList getHandlerList() {
-		return handlers;
-	}
+    /**
+     * Static method to get HandlerList.
+     * 
+     * @return HandlerList.
+     */
+    public static HandlerList getHandlerList() {
+        return handlers;
+    }
 
-	@Override
-	public HandlerList getHandlers() {
-		return handlers;
-	}
+    @Override
+    public HandlerList getHandlers() {
+        return handlers;
+    }
 
 }
