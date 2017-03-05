8b0af3f0e57451ad85d9bbb777156202d5a5da12
Unknown <hashemian.isf@gmail.com>
2017-03-05 14:58:28 +0330

    Permission nodes container added.
    class type: enum

---
+++ src/main/java/git/mmdhashemian/shrugypoints/permissions/PermissionNodes.java
@@ -0,0 +1,57 @@
+package git.mmdhashemian.shrugypoints.permissions;
+
+/**
+ * Class contain permission nodes
+ * 
+ * @author Mohammad_Hashemian
+ *
+ */
+public enum PermissionNodes {
+	LOOK(".look"),
+	GIVE(".give"),
+	GIVEALL(".giveall"),
+	TAKE(".take"),
+	TAKEALL(".takeall"),
+	SET(".set"),
+	SETALL(".setall"),
+	SPY(".spy"),
+	PAY(".pay"),
+	RESET(".reset"),
+	LEADERS(".leaders"),
+	RANK(".rank"),
+	ME(".me"),
+	SOUNDPLAY(".soundplay"),
+	FIREWORKRECEIVE(".fireworkreceive"),
+	BROADCAST(".broadcast"),
+	BROADCASTAUTO(".broadcastAUTO"),
+	RELOAD(".reload"),
+	UPDATE(".update");
+	
+	/**
+	 * Permission nodes field
+	 */
+	private String permissionnode;
+	
+	/**
+	 * Permission prefix
+	 */
+	private String prefix = "ShrugyPoints";
+	
+	/**
+	 * Private constructor
+	 * @param PermissionNode
+	 */
+	private PermissionNodes(String permnode) {
+		this.permissionnode = prefix + permnode;
+	}
+	
+	/**
+	 * Get the full permission path
+	 * @return permission node full path
+	 */
+	public String getNode() {
+		return permissionnode;
+	}
+	
+
+}
